8a1bf1dbf7d8f41c81fefbea696aaee21804187d
chenli <chenli@192.168.43.24>
2017-10-26 16:25:54 +0800

    new demos

--- Standard_SpringBoot/src/main/java/evolution/Application.java
+++ Standard_SpringBoot/src/main/java/evolution/Application.java
@@ -1,11 +1,21 @@
 package evolution;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	@Value("${name}")
+	private String name;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args).close();
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println(name);
 	}
 }
